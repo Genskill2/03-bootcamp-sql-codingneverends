1ac3359ab7b18083700b745170b8a58fb5ae9843
codingneverends <gaganlal7171@gmail.com>
2021-06-21 09:56:52 +0530

    del_update

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-DELETE FROM books_subjects WHERE subject in (SELECT id from subjects WHERE name="History");
+DELETE FROM books_subjects WHERE books_subjects.subject in (SELECT id from subjects WHERE name="History");
 DELETE FROM subjects WHERE name="History";

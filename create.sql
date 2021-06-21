9a9812253fd71bcaac9831ca4486b0484f41f781
codingneverends <gaganlal7171@gmail.com>
2021-06-21 10:09:36 +0530

    hmm

--- create.sql
+++ create.sql
@@ -3,15 +3,15 @@ CREATE TABLE publisher (
     name TEXT,
     country TEXT
 );
+CREATE TABLE subjects(
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
 CREATE TABLE books(
     id INTEGER PRIMARY KEY,
     title TEXT,
     publisher INTEGER,
     FOREIGN KEY (publisher) REFERENCES publisher(id)
-);
-CREATE TABLE subjects(
-    id INTEGER PRIMARY KEY,
-    name TEXT
 );
 CREATE TABLE books_subjects(
     book INTEGER,
